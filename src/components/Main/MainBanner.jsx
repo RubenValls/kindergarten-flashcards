6cb73d5500197f57cba267702692e6e45f71d252
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-12 11:25:23 +0200

    added redirection to game page

--- src/components/Main/MainBanner.jsx
+++ src/components/Main/MainBanner.jsx
@@ -1,15 +1,22 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function MainBanner() {
+    const navigate = useNavigate();
+
     useEffect(() => {
         let audio = document.getElementById("mainAudio");
         audio.volume = 0.20;
     }, []);
 
+    const handleClick = () => {
+        navigate('/game');
+    }
+
     return (
         <>
             <img src="./src/assets/mainBanner.png" alt="main-banner" id="main-banner"/>
-            <button id="main-btn">Let's Learn</button>
+            <button id="main-btn" onClick={handleClick}>Let's Learn</button>
             <audio src="./src/assets/music/backgroundMusic.mp3" autoPlay loop id="mainAudio"></audio>
         </>
     )

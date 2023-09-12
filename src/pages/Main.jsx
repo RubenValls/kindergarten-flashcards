06a887ccbde30cc292473eb2dc283357655020ec
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-12 10:44:46 +0200

    added main page with styles and background sound

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 export default function Main() {
+
+    useEffect(() => {
+        let audio = document.getElementById("mainAudio");
+        audio.volume = 0.25;
+    }, []);
+    
     return (
         <section id="main">
-            
+            <img src="./src/assets/mainBanner.png" alt="main-banner" id="main-banner"/>
+            <button id="main-btn">Let's Learn</button>
+            <audio src="./src/assets/music/backgroundMusic.mp3" autoPlay loop id="mainAudio"></audio>
         </section>
     );
 }

1369e138345eed2fec3b65a67686f3240b771ed9
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-12 09:54:09 +0200

    added main page and some styles to project

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ function App() {
   ]);
 
   return (
-    <>
+    <div className="app">
       <RouterProvider router={router} />
-    </>
+    </div>
   )
 }
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,7 @@
-
 export default function Main() {
-  return (
-    <div>Main</div>
-  )
+    return (
+        <section id="main">
+            
+        </section>
+    );
 }

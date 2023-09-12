6732f77c3a7d173f7b3940543dfc41cefb2a3202
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-12 09:41:14 +0200

    added react router and main page without style

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from "./pages/main";
+
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main/>,
+    },
+  ]);
 
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
   )
 }

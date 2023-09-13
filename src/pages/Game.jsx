cc62e31a982e00e389e0b131a7f2e84b4c3fd376
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-13 19:29:51 +0200

    solved error scrolling to top after rendering

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import Footer from "../components/Footer";
 import GameNav from "../components/Game/GameNav";
 import GameTopic from "../components/Game/GameTopic";
 
 export default function Game() {
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }, []);
 
     return (
         <section id="game">
--- src/pages/Topic.jsx
+++ src/pages/Topic.jsx
@@ -1,5 +1,5 @@
 import { useLocation } from "react-router-dom";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import TopicNav from "../components/Topic/TopicNav";
 import Subtopics from "../components/Topic/Subtopics";
 import Flashcards from "../components/Topic/Flashcards";
@@ -8,6 +8,14 @@ import Footer from "../components/Footer";
 export default function Topic() {
     const location = useLocation();
     const data = location?.state;
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }, []);
 
     return (
         <section id="topic">

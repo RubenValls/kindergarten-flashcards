de3879b2a622fcfe0831d1f65f2119a09847d399
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-13 19:03:57 +0200

    fixed some issues related with footer position

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,9 +4,11 @@ import MainBanner from "../components/Main/MainBanner";
 export default function Main() {
     
     return (
-        <section id="main">
-            <MainBanner/>
+        <>
+            <section id="main">
+                <MainBanner/>
+            </section>
             <Footer/>
-        </section>
+        </>
     );
 }

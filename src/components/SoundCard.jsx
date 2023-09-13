3aa084d4e945a1d18c07743b9161ab58f16e32cb
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-13 18:23:21 +0200

    added secret names before show it

--- src/components/SoundCard.jsx
+++ src/components/SoundCard.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 
 export default function SoundCard({name, img, sound}) {
+    const [title, setTitle] = useState(false);
     
     const playAudio = () => {
         const audio = new Audio(sound);
@@ -12,7 +14,16 @@ export default function SoundCard({name, img, sound}) {
                 <img src={img} alt={name}/>
             </div>
             <div className="topic-title">
-                <h2>{name.toUpperCase()}</h2>
+                {
+                    title
+                        ? (<h2>{name.toUpperCase()}</h2>)
+                        : (<h2 id="secretName">{'*'.repeat(name.length)}</h2>)
+                }
+                {
+                    title
+                        ? (<i className="fa-regular fa-eye-slash fa-lg" onClick={() => setTitle(!title)}></i>)
+                        : (<i className="fa-regular fa-eye fa-lg" onClick={() => setTitle(!title)}></i>)
+                }
             </div>
         </div>
     )
